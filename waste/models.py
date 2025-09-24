76862998477a625e656af64f570e73aa4b4e039a
palanikathirvel <palanikathirvel05@gmail.com>
2025-09-24 11:54:44 +0530

    Added Waste Module

---
+++ waste/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class WasteReport(models.Model):
+    description = models.TextField()
+    image = models.ImageField(upload_to='waste_reports/')
+    date_reported = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Report {self.id} - {self.date_reported}"
